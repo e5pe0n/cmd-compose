ebe04ec99e3f7c037872447e38450db0009b535a
e5pe0n <1izard.synchr0fi5h@gmail.com>
2024-07-14 22:25:42 +0900

    wip: depends_on

--- src/command.ts
+++ src/command.ts
@@ -13,3 +13,14 @@ export type Command = {
   depends_on: Command["name"][];
   log: (data: unknown) => void;
 };
+
+export function topoSort(commands: Command[]): Command[] {
+  const entryCandidates = commands.filter(
+    (command) => command.depends_on.length === 0
+  );
+  if (entryCandidates.length === 0) {
+    throw new Error(
+      "no-depends_on command does not exist. `commands` at least includes one command that has no `depends_on`."
+    );
+  }
+}
